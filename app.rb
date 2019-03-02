bb21b1bfaa527ccba0c928731189b6ccc7c7dffc
tatiana.mojica <tatiana.mojica@bustle.com>
2019-03-02 20:41:16 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,12 +19,12 @@ class App < Sinatra::Base
   end
 
   get '/set' do
-    session[:foo] == 'hello'
-    # if session[:foo] == 'hello'
-    #   redirect '/fetch'
-    # else
-    #   "Session value has not been set!"
-    # end
+    session[:foo] = 'hello'
+    if session[:foo] == 'hello'
+      redirect '/fetch'
+    else
+      "Session value has not been set!"
+    end
   end
 
   get '/fetch' do
